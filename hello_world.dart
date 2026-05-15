0966b0ccd4cd9cbe52f93961fbac4ec79eb7f381
MuEdgar <edgarmutangana@gmail.com>
2026-05-15 23:28:56 +0000

    more exercises

--- hello_world.dart
+++ hello_world.dart
@@ -1,4 +1,30 @@
 
 void main() {
-  print('Hello world');
+  print(rotateK([1,2,3,4,5,6,7], 3));
+}
+
+List<int> rotateK(List<int> nums, int k) {
+  if (nums.length == 0) return nums;
+
+  k = k % nums.length;
+
+  int n = nums.length - 1;
+
+  reverse(nums, 0, n);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, n);
+
+  return nums;
+}
+
+List<int> reverse(List<int> nums, int left, int right) {
+  while (left < right) {
+    int temp = nums[left];
+    nums[left] = nums[right];
+    nums[right] = temp;
+    left++;
+    right--;
+  }
+
+  return nums;
 }
