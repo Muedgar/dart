b180ced178f31e5f38d24e86c3bd17d71b36b7ee
MuEdgar <edgarmutangana@gmail.com>
2026-05-17 21:19:48 +0000

    more exercises

--- hello_world.dart
+++ hello_world.dart
@@ -1,6 +1,7 @@
 
 void main() {
-  print(rotateK([1,2,3,4,5,6,7], 3));
+  print('rotate k: ${rotateK([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 7)}');
+  print('cyclic k: ${rotateCyclic([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 7)}');
 }
 
 List<int> rotateK(List<int> nums, int k) {
@@ -24,6 +25,39 @@ List<int> reverse(List<int> nums, int left, int right) {
     nums[right] = temp;
     left++;
     right--;
+  }
+
+  return nums;
+}
+
+// rotate using cycles
+// pick item
+// find next index to place the picked item
+// 
+
+List<int> rotateCyclic(List<int> nums, int k) {
+  if (nums.length == 0) return nums;
+
+  int n = nums.length;
+
+  k = k % n;
+
+  int count = 0;
+
+  for (int start = 0; count < n; start++) {
+    int current = start;
+    int prev = nums[start];
+
+    do {
+      int next = (current + k) % n;
+      int temp = nums[next];
+
+      nums[next] = prev;
+      prev = temp;
+
+      current = next;
+      count++;
+    } while (start != current);
   }
 
   return nums;
